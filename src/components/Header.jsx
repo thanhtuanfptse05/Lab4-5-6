e254fa3b30fea42d5a3fa637994e056838931bc4
thanhtuanfptse05 <caotuan01122005@gmail.com>
2026-08-21 15:30:27 +0700

    update db

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 
 function Header() {
+  const account = JSON.parse(localStorage.getItem("account"));
   return (
     <div>
-      <div style={{ backgroundColor: "#d29422", color: "white" }}>
+      <div
+        style={{
+          backgroundColor: "#d29422",
+          color: "white",
+          display: "flex",
+          alignItems: "center",
+          padding: "15px",
+        }}
+      >
         <h2 className="d-flex text-start p-3">
           FPT Education Learning Materials Portal
         </h2>
+        {account && (
+          <span style={{ marginLeft: "auto" }}>Hello, {account.fullName}</span>
+        )}
       </div>
     </div>
   );
