9041272f39bfec7852a44dd2f19a8af67601bc57
thanhtuanfptse05 <caotuan01122005@gmail.com>
2026-08-24 11:45:44 +0700

    db

--- src/components/SubjectDetail.jsx
+++ src/components/SubjectDetail.jsx
@@ -1,98 +1,97 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
 
 function SubjectDetail() {
+  const { id } = useParams();
+  const [subject, setSubject] = useState([]);
+  useEffect(() => {
+    loadData();
+  }, [id]);
+  const navigate = useNavigate();
+  const loadData = async () => {
+    const subjectRes = await axios.get(`http://localhost:9000/subjects/${id}`);
+    setSubject(subjectRes.data);
+  };
+  const handleDelete = async (id) => {
+    const confirmDelete = window.confirm("xoa");
+
+    if (!confirmDelete) {
+      return;
+    }
+    await axios.delete(`http://localhost:9000/subjects/${id}`);
+
+    navigate("/syllabus");
+  };
   return (
     <div>
-      <div className="m-3">
-        <h3>Subject Information</h3>
+      <div className="row m-3" style={{ width: "35%" }}>
+        <label className="form-label" style={{ fontWeight: "bold" }}>
+          Code
+        </label>
+        <input type="text" className="form-control" value={subject.code} />
       </div>
+      <div className="row m-3" style={{ width: "35%" }}>
+        <label className="form-label" style={{ fontWeight: "bold" }}>
+          Name
+        </label>
+        <input type="text" className="form-control" value={subject.name} />
+      </div>
+      <div className="row m-3" style={{ width: "35%" }}>
+        <label className="form-label" style={{ fontWeight: "bold" }}>
+          Curriculum
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          value={subject.curriculum}
+        />
+      </div>
+      <div className="row m-3" style={{ width: "35%" }}>
+        <label className="form-label" style={{ fontWeight: "bold" }}>
+          Semester
+        </label>
+        <input type="text" className="form-control" value={subject.semester} />
+      </div>
+      <div className="row m-3" style={{ width: "35%" }}>
+        <label className="form-label" style={{ fontWeight: "bold" }}>
+          Credit
+        </label>
+        <input type="text" className="form-control" value={subject.credits} />
+      </div>
+      <div className="row m-3" style={{ width: "35%" }}>
+        <label className="form-label" style={{ fontWeight: "bold" }}>
+          Pre-requisites
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          value={subject.preRequisites}
+        />
+      </div>
+      <div className="row m-3" style={{ width: "35%" }}>
+        <label className="form-label" style={{ fontWeight: "bold" }}>
+          Description
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          value={subject.description}
+        />
+      </div>
+
       <div className="row m-3">
-        <div className="col-auto" style={{ width: "150px" }}>
-          <label className="col-form-label" style={{ fontWeight: "bold" }}>
-            Code
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            style={{ width: "300px" }}
-          />
+        <div className="col-auto">
+          <button className="btn btn-primary">Edit</button>
         </div>
-      </div>
-      <div className="row m-3">
-        <div className="col-auto" style={{ width: "150px" }}>
-          <label className="col-form-label" style={{ fontWeight: "bold" }}>
-            Name
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            style={{ width: "300px" }}
-          />
+        <div className="col-auto">
+          <button
+            className="btn btn-danger"
+            onClick={() => handleDelete(subject.id)}
+          >
+            Delete
+          </button>
         </div>
-      </div>
-      <div className="row m-3">
-        <div className="col-auto" style={{ width: "150px" }}>
-          <label className="col-form-label" style={{ fontWeight: "bold" }}>
-            Curriculum
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            style={{ width: "300px" }}
-          />
-        </div>
-      </div>
-      <div className="row m-3">
-        <div className="col-auto" style={{ width: "150px" }}>
-          <label className="col-form-label" style={{ fontWeight: "bold" }}>
-            Semester
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            style={{ width: "300px" }}
-          />
-        </div>
-      </div>
-      <div className="row m-3">
-        <div className="col-auto" style={{ width: "150px" }}>
-          <label className="col-form-label" style={{ fontWeight: "bold" }}>
-            Credit
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            style={{ width: "300px" }}
-          />
-        </div>
-      </div>
-      <div className="row m-3">
-        <div className="col-auto" style={{ width: "150px" }}>
-          <label className="col-form-label" style={{ fontWeight: "bold" }}>
-            Pre-requisites
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            style={{ width: "300px" }}
-          />
-        </div>
-      </div>
-      <div className="row m-3">
-        <div className="col-auto" style={{ width: "150px" }}>
-          <label className="col-form-label" style={{ fontWeight: "bold" }}>
-            Description
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            style={{ width: "300px" }}
-          />
-        </div>
-      </div>
-      <div className="d-flex m-3 justify-content-between">
-        <button>Edit</button>
-        <button>Delete</button>
       </div>
     </div>
   );
